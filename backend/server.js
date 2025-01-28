30cdffb15d6a534d9faf0fed2706cca544b907da
EsterIso <EsterlinjPaulino@gmail.com>
2025-01-28 12:48:54 -0500

    first commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+
+import productRoutes from './routes/product.route.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());//allow us to accept json data in the req.body
+
+app.use('/api/products', productRoutes)
+
+console.log(process.env.MONGODB_URI)
+
+app.listen(5000, () => {
+    connectDB();
+    console.log("Server started at http://localhost:5000");
+
+});
