473ea2d97452a796f34dcae8c244550042f57dbf
EsterIso <EsterlinjPaulino@gmail.com>
2025-01-28 12:58:23 -0500

    backend minor optimization

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import productRoutes from './routes/product.route.js';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());//allow us to accept json data in the req.body
 
@@ -14,8 +15,8 @@ app.use('/api/products', productRoutes)
 
 console.log(process.env.MONGODB_URI)
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server started at http://localhost:5000");
+    console.log("Server started at http://localhost:" + PORT);
 
 });
